e05a53efb9001d5376ab937cbda0c3c20ab328be
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-03-02 11:07:23 -0600

    namePrinter fn works

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('hi');
+
+
+// // firstand lastnames
+// // 
+
+// const firstName = 'Zoe';
+// const lastName = 'Ames';
+
+// //console.log(firstName + ' ' + lastName);
+// console.log(`${firstName}     ${lastName}`);
+
+// const firstName1 = 'Britney';
+// const lastName1 = 'Spears';
+
+// console.log(`${firstName} ${lastName}`)
+
+// const firstName2 = 'Tony';
+// const lastName2 = 'Romo';
+// console.log(`${firstName2} ${lastName2}`)
+
+const namePrinter = (firstName, lastName) => {
+
+console.log(`${firstName} ${lastName}`)
+};
+
+namePrinter('James', 'Brown');
+namePrinter('OH', 'YEAH!');
